7a2de13b8d0cce1eaaf41a5be7dd75ee87599483
Adalberto Maia <btowmaia@gmail.com>
2022-08-06 10:27:21 -0300

    f swiper

---
+++ mutlu/pages/[tenant]/index.tsx
@@ -0,0 +1,43 @@
+import { Banner } from '../../components/Banner';
+import { SearchInput } from '../../components/SearchInput';
+import styles from '../../styles/Home.module.css';
+
+const Home = () => {
+  const handleSearch = (searchValue: string) => {
+    console.log(`Oi ${searchValue}`)
+  }
+  return (
+    <div className={styles.container}>
+      <header className={styles.header}>
+        <div className={styles.headerTop}>
+          <div className={styles.headerTopLeft}>
+            <div className={styles.headerTitle}>
+              Seja Bem-Vindo(a) 👋
+            </div>
+            <div className={styles.headerSubtitle}> 
+              O que deseja para hoje? 
+            </div>
+          </div>
+          <div className={styles.headerTopRight}>
+            <div className={styles.menuButtom}>
+              <div className={styles.menuButtomLine}></div>
+              <div className={styles.menuButtomLine}></div>
+              <div className={styles.menuButtomLine}></div>
+            </div>
+
+          </div>
+        </div>
+        <div className={styles.headerBottom}>
+          <SearchInput 
+            mainColor='#FB9400'
+            onSearch={handleSearch}
+            />
+        </div>
+      </header>
+
+      <Banner />
+    </div>
+  )
+};
+
+export default Home;
