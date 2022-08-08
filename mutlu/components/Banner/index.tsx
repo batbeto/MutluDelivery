c4f81eb0ad9f331b407e3d04ad06deb0504f4ff4
Adalberto Maia <btowmaia@gmail.com>
2022-08-08 16:22:03 -0300

    swiper

--- mutlu/components/Banner/index.tsx
+++ mutlu/components/Banner/index.tsx
@@ -2,18 +2,20 @@ import styles from './styles.module.css';
 
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
+import { Autoplay } from 'swiper';
 
 export const Banner = () => {
   return (
     <div className={styles.container}>
       <Swiper
-        spaceBetween={50}
         slidesPerView={1}
+        loop={true}
         className={styles.swiper}
         autoplay = {{
           delay: 2500,
           disableOnInteraction: false,
         }}
+        modules={[Autoplay]}
       >
         <SwiperSlide className={styles.slide}><div className={styles.slideImg}>1</div></SwiperSlide>
         <SwiperSlide className={styles.slide}><div className={styles.slideImg}>2</div></SwiperSlide>
